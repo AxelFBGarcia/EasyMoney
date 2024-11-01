fd7d07cf09a88d1ee860ce8ec49a86f9ecce0d89
AxelFBGarcia <axel.becerril@wit.email>
2024-10-31 21:38:14 -0600

    Adición de pantalla de inicio

--- components/colors.tsx
+++ components/colors.tsx
@@ -1,7 +1,7 @@
 export const colors = {
   white: "#fff",
   primary: "#ef835d",
-  secondary: "#2db5aa",
+  secondary: "#17c3b3",
   tertiary: "#85c6d8",
   gray: "#d1d5db",
   graylight: "#F3F4F6",
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,6 +1,8 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
+import { StyleSheet } from "react-native";
+import BigText from "@/components/Texts/BigText";
 
 //Custom components
 import { Container } from "../components/shared";
@@ -29,22 +31,68 @@ const BottomSection = styled.View`
   width: 100%;
   flex: 1;
   padding: 25px;
+  justify-content: flex-end;
 `;
 
 // Image
 import background from "../assets/images/kurp2.jpg";
+import RegularButton from "@/components/Buttons/RegularButton";
+import SmallText from "@/components/Texts/SmallText";
 
 const Welcome = () => {
   return (
     <>
       <StatusBar style="light" />
       <WelcomeContainer>
-        <TopSection></TopSection>
-        <TopImage source={background}></TopImage>
-        <BottomSection>=</BottomSection>
+        <TopSection>
+          <TopImage source={background}></TopImage>
+        </TopSection>
+        <BottomSection>
+          <BigText textStyles={{ width: "70%", marginBottom: 25 }}>
+            Easy Money
+          </BigText>
+          <SmallText textStyles={{ width: "70%", marginBottom: 25 }}>
+            A money management app for tracking, optimizing, and receiving
+            updates on investment performance and growth.
+          </SmallText>
+          <RegularButton onPress={() => {}}>Get Started</RegularButton>
+        </BottomSection>
       </WelcomeContainer>
     </>
   );
 };
 
 export default Welcome;
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  topBox: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "lightgray",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headline: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginTop: 0,
+    width: 200,
+    height: 80,
+    backgroundColor: "yellow",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  otherContainer: {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green",
+  },
+});
